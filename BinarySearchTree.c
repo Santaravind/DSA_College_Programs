56786196f7f51b58f05dab01c78f14c6154fed09
Santaravind <aravindsant1310@gmail.com>
2026-04-02 11:36:40 +0530

    Traversal is add !!

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -10,10 +10,13 @@ struct BST *root=NULL;
 
 void insert();
 void InOrderDisplay(struct BST *);
+void preOrderDisplay(struct BST *);
+void postOrderDisplay(struct BST *);
+struct BST *tree_Search(struct BST *, int);
 int main(){
     int x;
     while(1){
-    printf(" \nEnter 1 to Add element in BST \nEnter 2 inorder traversal from BST\nEnter 4 for exit :-\nEnter value want to perform with queue:  ");
+    printf(" \nEnter 1 to Add element in BST \nEnter 2 inorder traversal from BST\nEnter 3 For pre order travesel\nEnter 4 for post order traversel\nEnter 6 for exit :-\nEnter value want to perform with queue:  ");
     scanf("%d",&x);
     
        switch(x){
@@ -21,10 +24,19 @@ int main(){
         insert();
        break;
       case 2:
+      printf("In order traversal:\t");
       InOrderDisplay(root);
        break;
+      case 3:
+      printf("Pre order traversal:\t");
+      preOrderDisplay(root);
+       break;
+      case 4:
+      printf("Post order traversal:\t");
+      postOrderDisplay(root);
+       break;
 
-       case 4:
+       case 6:
        exit(0);
        break;
 
@@ -76,11 +88,43 @@ void insert(){
     //     printf("BST not exixt\t");
     //     return;
     // }
+    // printf("Inorder traversal:\t");
      if(root!=NULL){
         InOrderDisplay(root->left);
         printf("%d ", root->key);
         InOrderDisplay(root->right);
 
+     }
+
+}
+  void preOrderDisplay(struct BST *root){
+    // struct BST *p;
+    // p=root;
+    // if(p==NULL){
+    //     printf("BST not exixt\t");
+    //     return;
+    // }
+    // printf("Pre order traversal:\t");
+     if(root!=NULL){
+      printf("%d ", root->key);
+        InOrderDisplay(root->left);       
+        InOrderDisplay(root->right);
+
+     }
+
+}
+  void postOrderDisplay(struct BST *root){
+    // struct BST *p;
+    // p=root;
+    // if(p==NULL){
+    //     printf("BST not exixt\t");
+    //     return;
+    // }
+  
+     if(root!=NULL){
+        InOrderDisplay(root->left);       
+        InOrderDisplay(root->right);
+        printf("%d ", root->key);
      }
 
 }
